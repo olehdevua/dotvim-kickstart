283e2bd79a1fa79a1c3b275e9b4a4d722d3e2314
olehdevua <olehdevua@gmail.com>
2025-01-15 14:30:59 +0200

    mine

--- init.lua
+++ init.lua
@@ -855,6 +855,7 @@ require('lazy').setup({
 
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
+      vim.api.nvim_set_hl(0, 'Normal', { bg = '#000000', fg = '#fffcfa' })
     end,
   },
 
