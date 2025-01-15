4cc6b72d417a3c621501893f70c798eb0a02c432
olehdevua <olehdevua@gmail.com>
2025-01-16 01:47:32 +0200

    start-plugin-configs

---
+++ lua/keymaps.lua
@@ -0,0 +1,164 @@
+-- [[ Basic Keymaps ]]
+--  See `:help vim.keymap.set()`
+
+-- Modes - mine
+--   normal_mode = "n",
+--   insert_mode = "i",
+--   visual_mode = "v",
+--   visual_block_mode = "x",
+--   term_mode = "t",
+--   command_mode = "c",
+
+--  `nore` mean NOT RECURSION .. for cases like `:inoremap ff fff`
+
+-- Clear highlights on search when pressing <Esc> in normal mode
+--  See `:help hlsearch`
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+
+-- Diagnostic keymaps
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+
+-- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
+-- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
+-- is not what someone will guess without a bit more experience.
+--
+-- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
+-- or just use <C-\><C-n> to exit terminal mode
+vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+
+-- TIP: Disable arrow keys in normal mode
+-- vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
+-- vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
+-- vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
+-- vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
+
+-- Keybinds to make split navigation easier.
+--  Use CTRL+<hjkl> to switch between windows
+--
+--  See `:help wincmd` for a list of all window commands
+-- vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+-- vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+-- vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+-- vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+
+-- [[ Basic Autocommands ]]
+--  See `:help lua-guide-autocommands`
+
+-- Highlight when yanking (copying) text
+--  Try it with `yap` in normal mode
+--  See `:help vim.highlight.on_yank()`
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
+
+---
+------ mine ---
+---
+
+-- inoremap jf <Esc>l
+vim.api.nvim_set_keymap('i', 'jf', '<Esc>l', { noremap = true })
+
+-- nnoremap <C-q><C-q> :qall!
+-- nnoremap <leader>qa :qall! <CR>
+-- nnoremap <leader>qq :q! <CR>
+vim.api.nvim_set_keymap('n', '<leader>qa', ':qall!<CR>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<leader>qq', ':q!<CR>', { noremap = true })
+-- nnoremap <C-b> :bp<CR>
+-- nnoremap <C-b> :e#<CR> " behavior of C-^
+vim.api.nvim_set_keymap('n', '<C-b>', ':e#<CR>', { noremap = true })
+
+-- vnoremap < <gv
+-- vnoremap > >gv
+vim.api.nvim_set_keymap('v', '>', '>gv', { noremap = true })
+vim.api.nvim_set_keymap('v', '<', '<gv', { noremap = true })
+
+-- nnoremap zl 33zl
+-- nnoremap zh 33zh
+vim.api.nvim_set_keymap('n', 'zl', '33zl', { noremap = true })
+vim.api.nvim_set_keymap('n', 'zh', '33zh', { noremap = true })
+
+--  https://www.youtube.com/watch?v=hSHATqh8svM
+--
+--  keep cursor at the same place
+-- nnoremap n nzzzv
+-- nnoremap N Nzzzv
+-- nnoremap J mzJ`z
+vim.api.nvim_set_keymap('n', 'n', 'nzzzv', { noremap = true })
+vim.api.nvim_set_keymap('n', 'N', 'Nzzzv', { noremap = true })
+vim.api.nvim_set_keymap('n', 'J', 'mzJ`z', { noremap = true })
+
+--  copy-to-the-end
+--  nnoremap Y y$
+--  v- to not capture cr-nl (:help g_)
+--nnoremap Y yg_
+vim.api.nvim_set_keymap('n', 'Y', 'yg_', { noremap = true })
+
+--  undo breackpoints
+vim.api.nvim_set_keymap('i', ',', ',<c-g>u', { noremap = true })
+vim.api.nvim_set_keymap('i', '.', '.<c-g>u', { noremap = true })
+vim.api.nvim_set_keymap('i', '!', '!<c-g>u', { noremap = true })
+vim.api.nvim_set_keymap('i', '?', '?<c-g>u', { noremap = true })
+vim.api.nvim_set_keymap('i', ';', ';<c-g>u', { noremap = true })
+vim.api.nvim_set_keymap('i', '{', '{<c-g>u', { noremap = true })
+vim.api.nvim_set_keymap('i', '[', '[<c-g>u', { noremap = true })
+vim.api.nvim_set_keymap('i', '"', '"<c-g>u', { noremap = true })
+vim.api.nvim_set_keymap('i', "'", "'<c-g>u", { noremap = true })
+
+--nnoremap <S-Left> :vertical resize -3<CR>
+--nnoremap <S-Right> :vertical resize +3<CR>
+vim.api.nvim_set_keymap('n', '<S-Left>', ':vertical resize +3<CR>', { noremap = true })
+vim.api.nvim_set_keymap('n', '<S-Right>', ':vertical resize -3<CR>', { noremap = true })
+
+-- :echom expand("%:h") . '/' . expand("%:t") . ':' . line(".")
+-- nnoremap <C-c><C-p> :echom expand("%:p") . ':' . line(".") <CR>
+--
+--nnoremap <leader>cp :let @+=expand("%:p") . ':' . line(".") <CR>
+vim.api.nvim_set_keymap('n', '<leader>cp', ':let @+=expand("%:p") . ":" . line(".")<cr>', { noremap = true, desc = 'Copy file path' })
+--  ^-- `+` is the name of register
+
+--  commented it because it prevent CTRL-I to work
+--  noremap <Tab> gt
+--  noremap <S-Tab> gT
+
+--inoremap "" ""<Left>
+--inoremap { {}<Left>
+vim.api.nvim_set_keymap('i', '""', '""<Left>', { noremap = true })
+vim.api.nvim_set_keymap('i', "''", "''<Left>", { noremap = true })
+vim.api.nvim_set_keymap('i', '``', '``<Left>', { noremap = true })
+vim.api.nvim_set_keymap('i', '<>', '<><Left>', { noremap = true })
+vim.api.nvim_set_keymap('i', '[', '[]<Left>', { noremap = true })
+vim.api.nvim_set_keymap('i', '(', '()<Left>', { noremap = true })
+vim.api.nvim_set_keymap('i', '{', '{}<Left>', { noremap = true })
+
+-- iabbrev cosnt const
+-- iabbrev ocnst const
+--
+-- iabbrev ;r return
+-- -- inoremap ;r return
+-- inoremap ;f function
+-- inoremap ;in instanceof
+-- inoremap ;i import
+-- inoremap ;if interface
+-- inoremap ;tn throw new
+-- inoremap ;q require
+-- inoremap ;e export
+-- inoremap ;a await
+-- inoremap ;ud undefined
+-- inoremap ;cs console
+-- inoremap ;co constructor
+-- inoremap ;c const
+-- inoremap ;df default
+-- inoremap ;x expect(
+-- inoremap ;P Promise
+--
+-- inoremap ;st static
+--
+-- inoremap ;s struct
+-- inoremap ;I Iterator
+-- inoremap ;O Option
+-- inoremap ;R Result
+-- inoremap ;pn println!("
---
+++ lua/options.lua
@@ -0,0 +1,138 @@
+-- :help vim.opt
+-- :help option-list
+
+-- Enable break indent
+-- vim.opt.breakindent = true
+
+-- Save undo history
+-- vim.opt.undofile = true
+
+-- default text encoding
+vim.opt.encoding = 'utf-8'
+vim.opt.termguicolors = true -- set term gui colors (most terminals support this)
+
+vim.opt.title = true
+vim.opt.showcmd = true
+vim.opt.number = true -- also vim.opt.relativenumber
+vim.opt.autoread = true -- For auto update files
+vim.opt.wildmenu = true
+vim.opt.showmatch = true -- highlight matching brace  (set sm)
+vim.opt.smartcase = true -- smart-case search
+vim.opt.ignorecase = true -- ignore registre while searching (set ic)
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.showmode = false -- don't show the mode, since it's already in the status line
+
+vim.opt.splitbelow = true -- force all horizontal splits to go below current window
+vim.opt.splitright = true -- force all vertical splits to go to the right of current window
+
+vim.cmd 'hi Comment cterm=italic'
+
+-- Sync clipboard between OS and Neovim.
+--  Schedule the setting after `UiEnter` because it can increase startup-time.
+--  Remove this option if you want your OS clipboard to remain independent.
+--  See `:help 'clipboard'`
+vim.schedule(function()
+  vim.opt.clipboard = 'unnamedplus'
+end)
+-- v- mine
+--
+-- set clipboard+=unnamedplus
+-- "set formatoptions-=cro " stop newline continution of comments (for now have no clue what does it means)
+-- vim.o.clipboard = 'unnamedplus'
+
+vim.opt.pumheight = 13 -- height of popup menu
+
+-- " https://habr.com/ru/post/64224/
+vim.opt.autoindent = true
+vim.opt.smartindent = true
+vim.opt.smarttab = true
+vim.opt.shiftwidth = 2
+vim.opt.softtabstop = 2
+vim.opt.tabstop = 2
+vim.opt.expandtab = true -- transformation tab into spaces
+vim.opt.wrap = false
+
+vim.o.cursorline = true
+vim.cmd 'hi CursorLine cterm=NONE ctermbg=234' -- color like in tmux
+
+-- highlight <word> when you do ':%s/<word>/..'
+vim.opt.inccommand = 'nosplit' -- nosplit is default (can be 'split')
+
+vim.opt.ruler = true -- Show row and column ruler information
+vim.opt.showtabline = 2 -- Show tab bar
+vim.opt.undolevels = 256 -- Number of undo levels
+-- Backspace behaviour
+vim.opt.backspace = 'indent,eol,start'
+
+-- by default `foldlevel` is 0, that is everything is
+-- folded, so you can to disable it initially
+--
+-- set nofoldenable
+vim.opt.foldmethod = 'indent'
+--
+-- fold everything below layer 2
+vim.opt.foldlevelstart = 2
+--
+vim.cmd 'hi Folded ctermbg=230'
+
+vim.opt.mouse = 'a'
+
+-- if has ("autocmd")
+--     filetype indent on
+-- endif
+
+-- set tags=tags;/,rusty-tags.vi
+
+-- Performance
+vim.opt.synmaxcol = 512
+vim.opt.lazyredraw = true -- to avoid scrolling problems
+
+-- :help 'list' and :help 'listchars'
+vim.opt.list = true
+-- vim.o.listchars = "tab:·\ " -- mine
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' } -- kickstart
+
+vim.opt.scrolloff = 14
+vim.opt.scrolljump = 14
+vim.opt.sidescrolloff = 36
+
+-- change sp window   map <Tab> <C-W>W:cd %:p:h<CR>:<CR>
+
+-- session options
+vim.opt.sessionoptions = 'curdir,buffers,tabpages'
+-- don't unload a buffe, when we are switching to another one.
+-- this allows edit several files at the same time without necessity
+-- to save every time we switch Не выгружать буфер
+--" Не выгружать буфер, когда переключаемся на другой. Это позволяет редактировать
+--" несколько файлов в один и тот же момент без необходимости сохранения каждый раз
+--" когда переключаешься между ними
+vim.opt.hidden = true
+
+--" set backupdir=~/.vim/backup//
+--" set directory=~/.vim/swap//
+vim.opt.backup = false
+vim.opt.writebackup = false
+-- disable creating swap files
+vim.opt.swapfile = false
+--"
+-- Having longer updatetime (default is 4000 ms = 4 s) leads to noticeable
+-- delays and poor user experience. (AFAIU better to disable swap files).
+-- btw: gives faster completion (4000ms default)
+vim.opt.updatetime = 200
+-- time to wait for a mapped sequence to complete (in milliseconds)
+-- didnt completely understood its meaning
+vim.opt.timeoutlen = 400
+-- vim.o.ttimeoutlen = 0 -- hz wo eto, razberis
+
+-- " set signcolumn=auto:2 " or number ?
+-- v-- or number ?
+vim.opt.signcolumn = 'yes:2'
+-- vim.opt.signcolumn = 'yes' -- kickstart
+
+--" give more space for displaying messages
+vim.opt.cmdheight = 2
+
+--" single status line for all windows
+vim.opt.laststatus = 3
